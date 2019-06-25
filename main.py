67ee3640c4a6a26ea75f9b5052ef0429253ff350
ahamedsajeer.15 <ahamedsajeer.15@cse.mrt.ac.lk>
2019-06-25 12:16:34 +0530

    adding latLang

--- main.py
+++ main.py
@@ -13,12 +13,17 @@ o = open("filtered_route.txt", "w+")
 tune=5
 
 a=list()
+b=list()
 for line in f:
-    a.append(float(line))
+    num=line.split(', ',1)
+    a.append(float(num[0]))
+    b.append(float(num[1]))
     if len(a)>tune:
         a.pop(0)
-    print (a,midValue(a))
-
+    if len(b)>tune:
+        b.pop(0)
+    print (str(midValue(a))+', '+str(midValue(b)))
+    o.write(str(midValue(a))+', '+str(midValue(b))+'\n')
 
 
 f.close()
